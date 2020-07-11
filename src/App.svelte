f5fb09c6841de76ff73ef5316422b74ac5ea180d
Platt Thompson <plattthompsonprogramming@gmail.com>
2020-07-10 19:02:41 -0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	export let name;
+
+	const tasks = ['wash car', 'mow lawn', 'cut hair'];
+</script>
+
+<main>
+	<h1>Hello {name}</h1>
+	<h2>To Do</h2>
+	<ul>
+		{#each tasks as task}
+			<li>{task}</li>
+		{/each}
+	</ul>
+</main>
+
+<style>
+	body {
+		background-color: black;
+	}
+
+	main {
+		width: 100vw;
+		height: 100vh;
+		background-color: darkslateblue;
+	}
+
+	h1 {
+		color: gray;
+	}
+</style>
